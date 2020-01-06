2cab6e634f2e4f8c2ec483adf99ed7b7eefd896d
moonjason <jasonmoonjm@gmail.com>
2020-01-05 23:56:30 -0800

    require schema in app

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql'); //variable is a naming convention of graphql (different requirement name vs variable)
 // this allows us to handle graphql requests on express ^^ 
+const schema = require('./schema/schema');
+
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-    
+    schema // same as schema: schema (ES6 ftw)
 })) // middleware for graphql endpoints
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql; //destructuring grabs that GraphQLObjectType from graphql requirement
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql; //destructuring grabs that GraphQLObjectType from graphql requirement
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -9,4 +9,23 @@ const BookType = new GraphQLObjectType({
         name: {type: GraphQLString },
         genre: {type: GraphQLString }
     }) // we do a function here to avoid reference errors when we have multiple types
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: {type: GraphQLString} }, // => book(id: '123') these are the arguments per book
+            resolve(parent, args) {
+                //code to get data from db / other source 
+                // parent comes in when we look at relationships between data
+                args.id 
+            }
+        }
+    } // doesn't need to be in a function cuz we don't care about order
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
