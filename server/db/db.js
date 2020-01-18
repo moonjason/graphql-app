345ae8576aa572dcf27ad8521ae43cf369ba6b30
moonjason <jasonmoonjm@gmail.com>
2020-01-18 10:36:46 -0800

    mongodb fix

--- server/db/db.js
+++ server/db/db.js
@@ -1,22 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
-const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://moonJason:test1234@graphql-practice-nt6ix.mongodb.net/test?retryWrites=true&w=majority'
+const uri = "mongodb+srv://<username>:<password>@graphql-practice-nt6ix.mongodb.net/test?retryWrites=true&w=majority";
 
-mongoose.connect(connectionString, { useNewUrlParser: true,
-                                     useUnifiedTopology: true,
-                                     useCreateIndex: true,
-                                     useFindAndModify: false
-                                    });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connection.on('connected', () => {
-console.log(`Mongoose connected to ${connectionString}`);
-});
-
-mongoose.connection.on('disconnected', () => {
-console.log('Mongoose disconnected');
-});
-
-mongoose.connection.on('error', (err) => {
-console.log('Mongoose error: ', err);
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('connected')
+  // perform actions on the collection object
+  client.close();
 });
