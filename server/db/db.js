dd315c08b5149d6888114b5c744fe5652f81092d
moonjason <jasonmoonjm@gmail.com>
2020-01-18 12:34:29 -0800

    change db stuff

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,22 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+// mongoose is an odm (object oriented data-model)
+const connectionString = "mongodb+srv://moonJason:test123@graphql-practice-nt6ix.mongodb.net/test?retryWrites=true&w=majority";
 
-const uri = "mongodb+srv://<username>:<password>@graphql-practice-nt6ix.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(connectionString, { useNewUrlParser: true,
+                                     useUnifiedTopology: true,
+                                     useCreateIndex: true,
+                                     useFindAndModify: false
+                                    });
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('connected')
-  // perform actions on the collection object
-  client.close();
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to GraphQL-Practice`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose error: ', err);
 });
