adcc4ac5132d58e6104c061bb840afd4d71f747e
moonjason <jasonmoonjm@gmail.com>
2020-01-18 17:55:44 -0800

    booklist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+//components
+import BookList from './components/BookList/BookList'
+
 function App() {
   return (
     <div id="main">
       <h1>GraphQL Practice - Reading List</h1>
+      <BookList/>
     </div>
   );
 }
---
+++ client/src/components/BookList/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BookList = () => {
+    return (
+        <div>
+            <ul id="booklist">
+                <li>Book Name</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookList;
